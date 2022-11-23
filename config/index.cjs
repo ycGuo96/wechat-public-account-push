31d23f2f16c39b29efbf386bb5b59a880b3e1cf6
ycGuo96 <38323333+ycGuo96@users.noreply.github.com>
2022-11-23 20:51:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       
-      id: 'oOPj15jukOGQFCrkY7ORN8GzQCgE',
+      id: 'oOPj15p4k5Txcx72XAv22_DhUKT4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '-XVdwhto981TQW6Ir5tO6Cv88DMfqfVU8j39wG9ACgY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
